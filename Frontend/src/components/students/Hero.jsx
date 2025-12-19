54c303980915f38a18227e8633616a7fa65c408f
Shashi895 <mr.shashiraj2014@gmail.com>
2025-12-19 16:37:50 +0530

    new

--- Frontend/src/components/students/Hero.jsx
+++ Frontend/src/components/students/Hero.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import SearchBar from './SearchBar'
 import Compaines from './Compaines'
+import sketch from '../../assets/sketch.png'
 
 const Hero = () => {
   return (
     <div className='flex flex-col items-center justify-center w-full md:pt-36 pt-20  px-7 md:px-0 space-y-7 text-center'>
-      <h1 className='text-lg md:text-3xl relative font-bold text-white max-w-3xl mx-auto uppercase'>“Empower your future with courses crafted for your”<span className='text-blue-600'>success.</span><img  src="/src/assets/sketch.png" alt="sketch" className='md:block hidden absolute bottom-9 right-0 w-[40%]' /></h1>
+      <h1 className='text-lg md:text-3xl relative font-bold text-white max-w-3xl mx-auto uppercase'>“Empower your future with courses crafted for your”<span className='text-blue-600'>success.</span><img  src={sketch} alt="sketch" className='md:block hidden absolute bottom-9 right-0 w-[40%]' /></h1>
       <p className='md:block hidden text-gray-300 max-w-2xl mx-auto'>We bring toghethr worls-class instructors, interactive content, and a supportive community to help you achive your personal and professional goal</p>
 
       <p className='md:hidden text-gray-500 max-w-sm mx-auto'>We bring tohether world-class instructors to help you professinal goals.</p>
--- Frontend/src/components/students/SearchBar.jsx
+++ Frontend/src/components/students/SearchBar.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
 import { useEffect,useState } from 'react'
-
+import search from '../../assets/search.png'
 const SearchBar = ({data}) => {
 
   const navigate=useNavigate()
@@ -15,7 +15,7 @@ const SearchBar = ({data}) => {
   return (
     
       <form onSubmit={onsearchhandler} className='max-w-xl w-full md:h-14 h-12 flex items-center bg-white border border-gray-500/20 rounded-full  ' action="">
-        <img src="/src/assets/search.png" alt="search icon" className='  w-15  px-3 ' />
+        <img src={search} alt="search icon" className='  w-15  px-3 ' />
         <input onChange={e=> setinput(e.target.value)}  value={input}type="text" placeholder='Seach for Courses' className=' w-full h-full outline-none text-gray-500/80'/>
         <button type='submit' className='bg-blue-600 rounded-full text-white md:px-10 px-7 md:py-3 py-2 mx-1 shadow-[0_0_25px_rgba(124,58,237,0.5)] cursor-pointer hover:bg-blue-700'>Search</button>
       </form>
